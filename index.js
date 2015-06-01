9eeb7e557b416b1a13f6a7cd9b27daebb27c5bc5
Kyle Shrader <kyle.r.shrader@gmail.com>
2015-06-01 01:08:20 -0700

    add naming

--- index.js
+++ index.js
@@ -10,14 +10,23 @@ app.get('/', function (req, res) {
     res.sendFile('index.html');
 });
 
+var users = 0;
 io.on('connection', function(socket) {
     console.log('a user connected');
+    var id = users++;
+    var name = '';
     socket.on('disconnect', function() {
         console.log('user disconnected');
     });
     socket.on('message sent', function(msg) {
-        console.log(msg);
-        io.emit('message received', msg);
+        if (!name) {
+            name = msg;
+            console.log('user ' + id + ' is named ' + name);
+            io.emit('message received', name + ' has connected');
+        } else {
+            console.log(name + ': ' + msg);
+            io.emit('message received', name + ': ' + msg);
+        }
     });
 });
 
